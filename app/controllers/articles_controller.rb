1879dabc03fb56cc939d22d38b7ec386ab7da384
Pazzik <andrej-bulygin@yandex.ru>
2015-09-27 19:37:36 +0300

    edit article ability

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,27 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
   def new
+  end
+
+  def edit
+   get_article
+  end
+
+  def show
+    get_article
+  end
+
+  def update
+    get_article
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -10,11 +32,14 @@ class ArticlesController < ApplicationController
     else
       render action: 'new'
     end
-
   end
 
   private
   def article_params
     params.require(:article).permit(:title, :text)
+  end
+
+  def get_article
+    @article = Article.find(params[:id])
   end
 end
