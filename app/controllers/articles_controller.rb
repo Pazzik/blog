fb8d2e2b10959facab970649aed7fa873137a073
Pazzik <andrej-bulygin@yandex.ru>
2015-09-27 20:09:38 +0300

    fix article validates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,8 +26,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    if @article.valid?
-      @article.save
+    if @article.save
       render action: 'create'
     else
       render action: 'new'
