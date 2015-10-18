22786348666eb20cb0a363030d02d62acef79fb7
Pazzik <andrej-bulygin@yandex.ru>
2015-10-18 14:02:29 +0300

    Add username to devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017144646) do
+ActiveRecord::Schema.define(version: 20151018102330) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "title"
@@ -58,9 +58,11 @@ ActiveRecord::Schema.define(version: 20151017144646) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
